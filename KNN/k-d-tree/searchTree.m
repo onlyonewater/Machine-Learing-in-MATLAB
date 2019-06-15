1bd6f64ceaa2ace842fd781d487dd57f77163ca6
onlyonewater <41667733+onlyonewater@users.noreply.github.com>
2019-06-15 16:54:13 +0800

    Update searchTree.m

--- KNN/k-d-tree/searchTree.m
+++ KNN/k-d-tree/searchTree.m
@@ -1,5 +1,5 @@
 function Nearest=searchTree(tree,point)
-%搜索树
+%搜索树，Nearest返回的是最近的数据点
 node_number=2;left_number=4;right_number=6;
 node=tree{1,node_number};    %节点
 Leaf=tree{1,left_number};     %默认是从左子树开始搜索
@@ -13,27 +13,23 @@ else      %按照y轴比较
     end
 end
 
-if iscell(Leaf)==1
-    Result=searchTree(Leaf,point);
-    dist=sqrt((node(1,1)-point(1,1))^2+(node(1,2)-point(1,2))^2);
-    %比较两个距离大小，取小
-    if Result{1,2}<dist
-        Nearest=Result{1,2};
+if iscell(Leaf)==1     %如果是cell类型的，说明还可以递归
+    Res=searchTree(Leaf,point);
+    dis=sqrt((node(1,1)-point(1))^2+(node(1,2)-point(2))^2);
+    dis0=sqrt((Res(1,1)-point(1))^2+(Res(1,2)-point(2))^2);
+    if dis0>dis
+        Nearest=[node(1,1),node(1,2)];
     else
-        Nearest={node(1:2),dist};
+        Nearest=[Res(1,1),Res(1,2)];
     end
 else
-    %判定是否为空值
-    if ~isempty(Leaf)
-        %计算两点的距离
-        dist1=sqrt((Leaf(1,1)-point(1,1))^2+(Leaf(1,2)-point(1,2))^2);
-        %计算与结点的距离
-        dist2=sqrt((node(1,1)-point(1,1))^2+(node(1,2)-point(1,2))^2);
-        %比较两个距离大小，取小
-        if dist1<dist2
-            Nearest={Leaf,dist1};
+    if isempty(Leaf)==0     %虽然是double类型的，但是里面还有数值
+        dis1=sqrt((node(1,1)-point(1))^2+(node(1,2)-point(2))^2);    %point与父节点的距离
+        dis2=sqrt((Leaf(1,1)-point(1))^2+(Leaf(1,2)-point(2))^2);    %point与子节点的距离
+        if dis1>dis2
+            Nearest=[Leaf(1,1),Leaf(1,2)];
         else
-            Nearest={node(1:2),dist2};
+            Nearest=[node(1,1),node(1,2)];
         end
     end
 end
